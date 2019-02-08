353efcb723eecc6c97bb2db96d545a44a174eced
kakorenko <kakorenko@mail.ru>
2019-02-08 22:35:46 +0300

    sortirovka

---
+++ bablsort/src/com/company/Main.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int[]a={-3,5,4,-8,1};
+	for (int i=0;i<a.length;i++){
+	    for(int j=a.length-1;j>i;j--){
+	        if(a[j]>a[j-1]){
+	            int tmp=a[j];
+	            a[j]=a[j-1];
+	            a[j-1]=tmp;
+            }
+        }
+    }
+	for(int i=0;i<a.length;i++){
+        System.out.println(a[i]);
+    }
+
+    }
+}
